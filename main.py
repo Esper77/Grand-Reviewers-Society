f6a1be995603a5ff56460e8760ac61a406a035f9
Esper77 <yo.moyo12355@gmail.com>
2023-03-18 23:58:44 +0300

    dded mailing algorythm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3 as sl
 import time
 import schedule
-from random import randint
+from random import choice
 from threading import Thread
 import telebot
 from telebot import types
@@ -12,7 +12,9 @@ def database_init():
     con.execute("""CREATE TABLE user (
         user_id INTEGER PRIMARY KEY,
         user_name TEXT DEFAULT User,
-        author_perm BOOL DEFAULT False
+        author_perm BOOL DEFAULT False,
+        moderator_perm BOOL DEFAULT False,
+        is_banned BOOL DEFAULT False
         );""")
     con.execute("""CREATE TABLE book (
         book_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -25,19 +27,16 @@ def database_init():
         content TEXT,
         approved BOOL DEFAULT False
         );""")
-    con.execute("""CREATE TABLE users_books (
-        book_id INTEGER NOT NULL,
-        user_id INTEGER NOT NULL,
-        started DATE NOT NULL
-        );""")
+    con.execute("INSERT INTO book (book_name, author_id) values (?, ?)", ("Над пропастью во ржи", "NO"))
     con.commit()
+# database_init()
+
 
 TOKEN = "5880785142:AAEU12-MT3jdVPk6M5reRQvEIFG3-QOABtk"
 bot = telebot.TeleBot(token=TOKEN)
 
-# This is a start module
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["start"])  # This is a start module
 def start(message):
     con = sl.connect("application.db")
     chat_id = message.chat.id
@@ -46,5 +45,46 @@ def start(message):
         con.execute('INSERT INTO user (user_id, user_name) values (?, ?)', (chat_id, "User"))
         con.commit()
 
+
+@bot.message_handler(commands=["force"])
+def forced_mailing(message):
+    mailing_send()
+
+# Blacklist mode needed here
+
+
+@bot.message_handler(commands=["add-book"])
+def book_add(message):
+    con = sl.connect("application.db")
+    chat_id = message.chat.id
+
+
+def mailing_send():  # Mailing module
+    con = sl.connect("application.db")
+    users_data = [user_info[0] for user_info in con.execute("SELECT user_id FROM user WHERE NOT is_banned")]
+    books_data = [book for book in con.execute("SELECT book_id, book_name FROM book")]
+    for user_info in users_data:
+        keyboard = types.InlineKeyboardMarkup()
+        chosen_books = set()
+        while len(chosen_books) < 5:
+            chosen_books.add(choice(books_data))
+            break  # !This is not a solution to the lack of books
+        for book in list(chosen_books):
+            button1 = types.InlineKeyboardButton(text=f"{book[1]}", callback_data=book[0])
+            keyboard.add(button1)
+        bot.send_message(user_info, "Выберите книгу", reply_markup=keyboard)
+
+
+def mailing_check():
+    schedule.every().saturday.at('19:00').do(mailing_send)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        print('OK')
+
+
+remind_thread = Thread(target=mailing_check)
+
+remind_thread.start()
 
 bot.polling()
