98fe8ec09b08fda2a2577c199ff60cac59c9119f
Esper77 <yo.moyo12355@gmail.com>
2023-04-01 22:48:54 +0300

    Huge update

--- main.py
+++ main.py
@@ -43,6 +43,12 @@ class ReviewLibrary:
     def update_specific(self, user_id, content):
         self._conn.execute("UPDATE review SET content = (?) WHERE user_id = (?) AND is_closed = 0", (content, user_id))
 
+    def approve(self, user_id):
+        self._conn.execute("UPDATE review SET approved = 1 WHERE user_id = (?) AND approved = 0", (user_id, ))
+
+    def get_approval(self, approval=False):
+        return [" ".join(x) for x in self._conn.execute("SELECT user_id, book_id FROM review WHERE approved = ?", (approval,))]
+
 
 class UserLibrary:
     def __init__(self, conn):
@@ -62,6 +68,17 @@ class UserLibrary:
         for perm in range(1, len(perms)):
             request += f" AND {perms[perm]}=1"
         return [x[0] for x in self._conn.execute(request)]
+
+    def perm_grant(self):
+        self._conn.execute('UPDATE user SET moderator_perm = 1 WHERE exp>100')
+        self._conn.execute('UPDATE user SET author_perm = 1 WHERE exp>150')
+
+    def get_exp(self, user_id):
+        return self._conn.execute('SELECT exp FROM user WHERE user_id = (?)', (user_id, ))[0]
+
+    def give_exp(self, user_id):
+        current_exp = self.get_exp(user_id)
+        return self._conn.execute('UPDATE user SET exp = (?) WHERE user_id = (?)', (current_exp + 10, user_id))
 
 
 def get_connection():
@@ -83,6 +100,7 @@ def database_init(con):
     con.execute("""CREATE TABLE user (
         user_id INTEGER PRIMARY KEY,
         user_name TEXT DEFAULT User,
+        exp INT DEFAULT 0
         author_perm BOOL DEFAULT False,
         moderator_perm BOOL DEFAULT False,
         is_banned BOOL DEFAULT False
@@ -93,6 +111,7 @@ def database_init(con):
         author_id INTEGER NOT NULL
         );""")
     con.execute("""CREATE TABLE review (
+        review_id INTEGER NOT NULL AUTOINCREMENT,
         book_id INTEGER NOT NULL,
         user_id INTEGER NOT NULL,
         content TEXT,
@@ -102,7 +121,7 @@ def database_init(con):
     con.execute("INSERT INTO book (book_name, author_id) values (?, ?)", ("Над пропастью во ржи", "NO"))
 
 
-# database_init()
+database_init()
 
 
 @with_connection
@@ -141,6 +160,14 @@ def book_add(con, message):
 
 
 @with_connection
+@bot.message_handler(commands=["exp"])
+def get_exp(con, message):
+    chat_id = message.chat.id
+    experience = UserLibrary(con).get_exp(chat_id)
+    bot.send_message(chat_id, f"Ваш опыт: {experience}")
+
+
+@with_connection
 def book_confirmed(con, message):
     library = BookLibrary(con)
     text = message.text
@@ -154,18 +181,28 @@ def book_confirmed(con, message):
 @with_connection
 def callback_operating(con, call):  # This is actually callback operating module
     library = ReviewLibrary(con)
-    callback = int(call.data)
+    callback = call.data
     chat_id = call.message.chat.id
-    if callback in BookLibrary(con).get_ids():
-        if (callback, chat_id) not in library.get_ids():
-            library.add(chat_id, callback)
-            print(f"Review added ids:{callback, chat_id}")
-            ans = bot.send_message(chat_id, "Добавление рецензии\nНапишите вашу рецензию следующим сообщением")
+    try:
+        if int(callback) in BookLibrary(con).get_ids():
+            callback = int(callback)
+            if (callback, chat_id) not in library.get_ids():
+                library.add(chat_id, callback)
+                print(f"Review added ids:{callback, chat_id}")
+                ans = bot.send_message(chat_id, "Добавление рецензии\nНапишите вашу рецензию следующим сообщением")
+            else:
+                library.open(chat_id, callback)
+                print(f"Review update request ids: {callback, chat_id}")
+                ans = bot.send_message(chat_id, "Обновление рецензии\nНапишите вашу рецензию следующим сообщением")
+            bot.register_next_step_handler(ans, operate_review)
+    except TypeError:
+        callback = callback.split()
+        if callback[1] == "True":
+            UserLibrary(con).give_exp(int(callback[0]))
+            ReviewLibrary(con).approve(int(callback[0]))
         else:
-            library.open(chat_id, callback)
-            print(f"Review update request ids: {callback, chat_id}")
-            ans = bot.send_message(chat_id, "Обновление рецензии\nНапишите вашу рецензию следующим сообщением")
-        bot.register_next_step_handler(ans, operate_review)
+            ans = bot.send_message(callback[0], "Ваша рецензия не была принята, попробуйте её пересмотреть")
+            bot.register_next_step_handler(ans, operate_review)
 
 
 @with_connection
@@ -177,13 +214,34 @@ def operate_review(con, message):
     print("Review content added, data:", (chat_id, text))
     library.close(chat_id)
 
+    approval_send(text, chat_id)
 
-def mailing_check():
+
+@with_connection
+def approval_send(con, text, chat_id):
+    library = UserLibrary(con)
+    target_chat = choice(library.get_with_perm(['moderator_perm']))
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton(text=f"Хорошая", callback_data=f"{chat_id} True")
+    keyboard.add(button1)
+    button2 = types.InlineKeyboardButton(text=f"Плохая", callback_data=f"{chat_id} False")
+    keyboard.add(button2)
+    bot.send_message(target_chat, "Рецензия на проверку:\n" + text, reply_markup=keyboard)
+
+
+def check():
     schedule.every().saturday.at('19:00').do(mailing_send)
+    perm_granting()
     while True:
         schedule.run_pending()
         time.sleep(1)
         print('OK')
+
+
+@with_connection
+def perm_granting(con):
+    library = UserLibrary(con)
+    library.perm_grant()
 
 
 @with_connection
@@ -196,15 +254,14 @@ def mailing_send(con):  # Mailing module
         chosen_books = set()
         while len(chosen_books) < 5:
             chosen_books.add(choice(books_data))
-            break  # !This is not a solution to the lack of books
         for book in list(chosen_books):
             button1 = types.InlineKeyboardButton(text=f"{book[1]}", callback_data=book[0])
             keyboard.add(button1)
         bot.send_message(user_info, "Выберите книгу", reply_markup=keyboard)
 
 
-remind_thread = Thread(target=mailing_check)
+passive_thread = Thread(target=check)
 
-remind_thread.start()
+mailing_thread.start()
 
 bot.polling()
